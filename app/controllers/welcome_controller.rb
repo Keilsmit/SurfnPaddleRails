a6c5c7803ee872336b327e2cc4c80d44c1135103
keilsmit <keilsmit@yahoo.com>
2016-10-21 16:11:21 -0400

    I did it-Explorer

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,13 @@
 class WelcomeController < ApplicationController
 
   def index
+    @post = Post.all.max_by {|post| post.created_at}
+    @bottom_post = Post.all.reject {|post| post == @post }
   end
 
   def show
+    @post = Post.find(params[:id].to_i)
+    @bottom_post = Post.all.reject {|post| post == @post }
   end
 
 
